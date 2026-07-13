9aca29692d6fc40bcaf0a8531301eadac3e12ed9
padthe <n.patrick040@gmail.com>
2026-07-13 21:25:09 +0200

    Start of project, atleast a lamp is blinking now

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_rp::gpio::{Level,Output};
+use embassy_time::Timer;
+use panic_halt as _;
+
+#[embassy_executor::main]
+async fn main(__spawner: Spawner){
+    let peripherals = embassy_rp::init(Default::default());
+
+    let mut led = Output::new(peripherals.PIN_16, Level::Low);
+
+    loop {
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
 }
