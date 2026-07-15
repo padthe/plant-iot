325396aa3a0a758eac6c468effef2a6867b25268
padthe <n.patrick040@gmail.com>
2026-07-15 19:40:51 +0200

    added new depencencies and other before soon implementing wifi connection

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -2,12 +2,15 @@
 #![no_main]
 
 use embassy_executor::Spawner;
-use embassy_rp::gpio::{Level,Output};
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, DMA_CH1, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::{bind_interrupts, dma};
 use embassy_time::Timer;
 use panic_halt as _;
 
 #[embassy_executor::main]
-async fn main(__spawner: Spawner){
+async fn main(__spawner: Spawner) {
     let peripherals = embassy_rp::init(Default::default());
 
     let mut led = Output::new(peripherals.PIN_16, Level::Low);
